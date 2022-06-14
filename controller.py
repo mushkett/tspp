cb2bf7b5b036e1f630b6044304ca5364b93114c5
mushkett <lebedka.dmitry@gmail.com>
2022-06-14 17:17:11 +0300

    Add admin interface and authorization. Add save_to_word function

--- controller.py
+++ controller.py
@@ -29,33 +29,41 @@ def get_flights(plane_number, hours):
             result = session.query(Flight)\
                 .filter(Flight.hours < int(hours))\
                 .filter(Flight.plain_number == plane_number)\
+                .order_by(Flight.id)\
                 .all()
 
         elif hours:
             result = session.query(Flight)\
-                .filter(Flight.hours >= int(hours))\
+                .filter(Flight.hours >= int(hours)) \
+                .order_by(Flight.id)\
                 .all()
         elif plane_number:
             result = session.query(Flight)\
-                .filter(Flight.plain_number == plane_number)\
+                .filter(Flight.plain_number == plane_number) \
+                .order_by(Flight.id) \
                 .all()
+        else:
+            result = session.query(Flight).order_by(Flight.id).all()
 
     except DataError:
         session.rollback()
-        return ['Нічого не знайдено']
+        Sg.Popup('Виникла помилка! Перевірте правильність введених даних!', title="Error")
     else:
+        if result:
+            list_of_flights = []
 
-        list_of_flights = []
-
-        for i in result:
-            temp_list = []
-            i_dict = i.__dict__
-            temp_list.append(i_dict['plain_number'])
-            temp_list.append(i_dict['hours'])
-            temp_list.append(i_dict['route_number'])
-            list_of_flights.append(temp_list)
-        session.close()
-        return list_of_flights
+            for i in result:
+                temp_list = []
+                i_dict = i.__dict__
+                temp_list.append(i_dict['id'])
+                temp_list.append(i_dict['plain_number'])
+                temp_list.append(i_dict['hours'])
+                temp_list.append(i_dict['route_number'])
+                list_of_flights.append(temp_list)
+            session.close()
+            return list_of_flights
+        else:
+            Sg.Popup('Нічого не знайдено!', title='Error')
 
 
 def get_plane_list():
@@ -68,17 +76,19 @@ def save_to_docx(list_of_flights):
     document = Document()
     document.add_heading("Flights", 1)
     if list_of_flights:
-        table = document.add_table(rows=1, cols=3)
+        table = document.add_table(rows=1, cols=4)
         hdr_cells = table.rows[0].cells
-        hdr_cells[0].text = "Номер літака"
-        hdr_cells[1].text = "Кількість льотних годин"
-        hdr_cells[2].text = "Номер рейсу"
+        hdr_cells[0].text = 'ID'
+        hdr_cells[1].text = "Номер літака"
+        hdr_cells[2].text = "Кількість льотних годин"
+        hdr_cells[3].text = "Номер рейсу"
 
         for item in list_of_flights:
             row_cells = table.add_row().cells
-            row_cells[0].text = item[0]
-            row_cells[1].text = str(item[1])
-            row_cells[2].text = item[2]
+            row_cells[0].text = str(item[0])
+            row_cells[1].text = item[1]
+            row_cells[2].text = str(item[2])
+            row_cells[3].text = item[3]
         document.save("Flights.docx")
     else:
         return Sg.Popup("Flights not found")
@@ -99,3 +109,25 @@ def authorization():
     else:
         Sg.Popup("Incorrect login or password", title="Error")
     return is_authorized
+
+
+def add_flight(flight_id, plain_number, hours, route_number):
+    session = create_session()
+    if flight_id:
+        flight = session.query(Flight).filter(Flight.id == int(flight_id))
+        record = flight.one()
+        record.plain_number = plain_number
+        record.hours = int(hours)
+        record.route_number = route_number
+        Sg.Popup('Рейс успішно оновлено', title='Success')
+
+    else:
+        new_flight = Flight(
+            plain_number=plain_number,
+            hours=hours,
+            route_number=route_number
+        )
+        session.add(new_flight)
+        Sg.Popup('Рейс успішно додано', title='Success')
+
+    session.commit()
--- view.py
+++ view.py
@@ -1,65 +1,116 @@
 import PySimpleGUI as Sg
-from controller import get_flights, get_plane_list, save_to_docx, authorization
+from controller import get_flights, get_plane_list, save_to_docx, authorization, add_flight
 
 
 def main_window():
-    flights = []
     is_authorized = False
+    flights = get_flights('', '')
+    plane_list = get_plane_list()
+    plane_list.append('')
+
+    is_adding = True
     Sg.theme("DarkAmber")
 
-    search = [[Sg.Text('Номер літака'), Sg.OptionMenu(get_plane_list())],
+    search = [[Sg.Text('Номер літака'), Sg.OptionMenu(plane_list)],
               [Sg.Text('Кількість льотних годин'), Sg.InputText('')],
               [Sg.Button('Вибрати')],
               ]
 
-    table = [
+    table_admin = [
         Sg.Table(
-            headings=['Номер літака', 'Кількість льотних годин', 'Номер рейсу'],
-            values=[
-                []
-            ], key='-table-'
+            headings=['id', 'Номер літака', 'Кількість льотних годин', 'Номер рейсу'],
+            values=flights,
+            key='-table_admin-',
+            enable_click_events=True,
+            enable_events=True,
+            select_mode=Sg.TABLE_SELECT_MODE_BROWSE
         )
     ]
 
-    col1 = [[Sg.Button("Зберегти в файл")],
-            [Sg.Frame('Пошук рейсів',
-                      layout=[
-                          [Sg.Column(search, element_justification='c')]
-                      ]
-                      )
-             ]
-            ]
-
-    col2 = [[Sg.Button('Рейси', visible=False)]]
-
-    col3 = [
-        [Sg.Button('Пошук рейсів')],
-        table,
-
+    table_user = [
+        Sg.Table(
+            headings=['id', 'Номер літака', 'Кількість льотних годин', 'Номер рейсу'],
+            values=flights,
+            key='-table-user'
+        )
     ]
 
-    col4 = [[Sg.Button('Авторизація')]]
+    col1_user = [[Sg.Button("Зберегти в файл")],
+                 [Sg.Frame('Пошук рейсів',
+                           layout=[
+                               [Sg.Column(search, element_justification='c')]
+                           ]
+                           )
+                  ]
+                 ]
+
+    col2_user = [[Sg.Button('Пошук рейсів')]]
+
+    col3_user = [[Sg.Button('Авторизація')],
+                 table_user]
+
+    col1_admin = [
+        [Sg.Button("Зберегти в файл")],
+        table_admin
+    ]
+
+    col2_admin = [
+        [Sg.Button("Рейси")],
+        [Sg.Button('+', disabled=True, key='+')],
+        [Sg.Button('/', disabled=False, key='/')]
+    ]
+
+    col3_admin = [
+        [Sg.Button('Вийти')],
+        [Sg.Frame('Рейс', layout=[
+            [Sg.Text('ID'), Sg.InputText('', disabled=True, key='-id-')],
+            [Sg.Text('Номер літака'), Sg.InputText('', key='-plane-')],
+            [Sg.Text('Кількість льотних годин'), Sg.InputText('', key='-hours-')],
+            [Sg.Text('Номер маршруту'), Sg.InputText('', key='-route-')],
+            [Sg.Button('Зберегти')],
+        ])
+         ]
+    ]
 
     layout = [[
         Sg.Column(
-            col1,
+            col1_user,
+            key="-COL-1-USER",
+            vertical_alignment='t',
+            element_justification='left'
+        ),
+        Sg.Column(
+            col1_admin,
+            key="-COL-1-ADMIN",
+            vertical_alignment='t',
+            element_justification='left',
+            visible=False
+        ),
+        Sg.Column(
+            col2_user,
+            key="-COL-2-USER",
             vertical_alignment='t',
             element_justification='c'
         ),
         Sg.Column(
-            col2,
+            col2_admin,
+            key="-COL-2-ADMIN",
             vertical_alignment='t',
-            element_justification='c'
+            element_justification='left',
+            visible=False
         ),
         Sg.Column(
-            col3,
+            col3_user,
+            key="-COL-3-USER",
             vertical_alignment='t',
-            element_justification='c'
+            element_justification='right',
         ),
         Sg.Column(
-            col4,
+            col3_admin,
             vertical_alignment='t',
-            element_justification='c'
+            element_justification='right',
+            key="-COL-3-ADMIN",
+            visible=False
         )
     ]]
 
@@ -69,17 +120,64 @@ def main_window():
         event, values = window.read()
         if event == Sg.WINDOW_CLOSED or event == "Cancel":
             break
+
         elif event == "Вибрати":
             flights = get_flights(values[0], values[1])
-            window.Element('-table-').update(flights)
+            window.Element('-table-user').update(flights)
+
         elif event == "Зберегти в файл":
             save_to_docx(flights)
+
         elif event == "Авторизація":
             is_authorized = authorization()
             if is_authorized:
-                pass
+                window.Element('-COL-1-USER').update(visible=False)
+                window.Element('-COL-2-USER').update(visible=False)
+                window.Element('-COL-3-USER').update(visible=False)
 
-        print('you entered ', values[0], ' ', values[1])
+                window.Element('-COL-1-ADMIN').update(visible=True)
+                window.Element('-COL-2-ADMIN').update(visible=True)
+                window.Element('-COL-3-ADMIN').update(visible=True)
+
+        elif event == 'Вийти':
+            is_authorized = False
+            window.Element('-COL-1-USER').update(visible=True)
+            window.Element('-COL-2-USER').update(visible=True)
+            window.Element('-COL-3-USER').update(visible=True)
+
+            window.Element('-COL-1-ADMIN').update(visible=False)
+            window.Element('-COL-2-ADMIN').update(visible=False)
+            window.Element('-COL-3-ADMIN').update(visible=False)
+
+        elif event == '-table_admin-':
+            try:
+                data_selected = [flights[row] for row in values[event]][0]
+            except IndexError:
+                continue
+            if not is_adding:
+                print(data_selected)
+                window.Element('-id-').update(data_selected[0])
+            window.Element('-plane-').update(data_selected[1])
+            window.Element('-hours-').update(data_selected[2])
+            window.Element('-route-').update(data_selected[3])
+
+        elif event == "/":
+            window.Element('-id-').update(disabled=False)
+            window.Element('+').update(disabled=False)
+            window.Element('/').update(disabled=True)
+            is_adding = False
+
+        elif event == "+":
+            window.Element('-id-').update('', disabled=True)
+            window.Element('+').update(disabled=True)
+            window.Element('/').update(disabled=False)
+            is_adding = True
+
+        elif event == 'Зберегти':
+            add_flight(values['-id-'], values['-plane-'], values['-hours-'], values['-route-'])
+
+            flights = get_flights('', '')
+            window.Element('-table_admin-').update(flights)
 
 
 main_window()
